58afd1d64df40d1a3c0ee440bda68be788c6508a
Francois Achache <francois.achache@gmail.com>
2017-08-19 05:52:54 +0200

    add jsdoc

--- src/pdfjsbox.pdfview.js
+++ src/pdfjsbox.pdfview.js
@@ -22,14 +22,15 @@
 			},
 			link: function (scope, elm, attrs, ctrl) {
 				var watcherClears = [];
+				// Don't survey ngItem because, ngItem.items cause infinitive loop
 				watcherClears.push(scope.$watchGroup(['ngItem.document', 'ngItem.pageIdx', 'ngItem.rotate', 'ngScale'], function (vs1, vs2, s) {
-					updateView(s, s.ctrl, elm, s.ngItem, s.ngScale);
+					updateView(s.ctrl, elm, s.ngItem, s.ngScale);
 				}), true);
 				pdfjsboxWatcherServices.cleanWatchersOnDestroy(scope, watcherClears);
-				updateView(scope, ctrl, elm, scope.ngItem, scope.ngScale);
+				updateView(ctrl, elm, scope.ngItem, scope.ngScale);
 			}
 		};
-		function updateView(scope, ctrl, elm, item, scale) {
+		function updateView(ctrl, elm, item, scale) {
 			if (item) {
 				elm.addClass('notrendered');
 				item.getPage().then(function (pdfPage) {
@@ -41,35 +42,43 @@
 		}
 		/**
 		 * Dessine la page du pdf dans elm, elm etant un pdf-view 
-		 * @param {type} ctrl
-		 * @param {type} elm
-		 * @param {type} pdfPage
-		 * @param {type} rotate
-		 * @param {type} scale
-		 * @param {type} render
-		 * return 
+		 * @param {Angular Ctrl} ctrl
+		 * @param {JQueryElement} pdfView
+		 * @param {PDFPage} pdfPage
+		 * @param {int} rotate
+		 * @param {float} scale
+		 * @param {boolean} render
 		 */
-		function drawPdfPageToView(ctrl, elm, pdfPage, rotate, scale, render) {
-			clearTextLayer(elm);
-			var ctx = getAndclearCanvasContext(elm);
+		function drawPdfPageToView(ctrl, pdfView, pdfPage, rotate, scale, render) {
+			clearTextLayer(pdfView);
+			var ctx = getAndClearCanvasContext(pdfView);
 			if (ctx) {
 				var viewport = getViewport(pdfPage, scale, rotate);
-				defineSizes(elm, viewport.width, viewport.height);
+				defineSizes(pdfView, viewport.width, viewport.height);
 				if (pdfPage) {
-					elm.addClass('notrendered');
+					pdfView.addClass('notrendered');
 					if (render) {
 						ctrl.readyToRender().then(function () {
-							drawPageToCtx(ctrl, elm, pdfPage, viewport, ctx);
+							drawPageToCtx(ctrl, pdfView, pdfPage, viewport, ctx);
 						});
 					}
 				}
 			}
 		}
-		function drawPageToCtx(ctrl, elm, pdfPage, viewport, ctx) {
+		/**
+		 * Dessine la page dans le context du canvas
+		 * @param {Angular Ctrl} ctrl
+		 * @param {JQueryElement} pdfView
+		 * @param {PDFPage} pdfPage
+		 * @param {ViewPort} viewport
+		 * @param {CanvasContext} ctx
+		 * @returns {Promise}
+		 */
+		function drawPageToCtx(ctrl, pdfView, pdfPage, viewport, ctx) {
 			return pdfPage.render({canvasContext: ctx, viewport: viewport}).promise.then(function () {
-				elm.removeClass('notrendered');
-				ctrl.setReadyToRender();
-				var textLayer = elm.find('.textLayer').get(0);
+				pdfView.removeClass('notrendered');
+				ctrl.setReadyToRender(); // emulate mutex unlock
+				var textLayer = pdfView.find('.textLayer').get(0);
 				if (textLayer) {
 					return pdfPage.getTextContent().then(function (textContent) {
 						PDFJS.renderTextLayer({
@@ -83,15 +92,33 @@
 				return null;
 			});
 		}
+		/**
+		 * 
+		 * @param {PDFPage} pdfPage
+		 * @param {float} scale
+		 * @param {float} rotate
+		 * @returns {ViewPort or Object support width and height}
+		 */
 		function getViewport(pdfPage, scale, rotate) {
 			return pdfPage ? pdfPage.getViewport(scale || 1, rotate || 0) : {width: 0, height: 0};
 		}
-		function defineSizes(elm, width, height) {
-			elm.find('canvas').attr('width', width).attr('height', height);
-			elm.find('.page,.canvasWrapper,.textLayer').css('width', width + 'px').css('height', height + 'px');
+		/**
+		 * Definit la taille des different conteneurs : canvas, .page, .canvasWrapper, .textLayer
+		 * @param {JQueryElement} pdfView
+		 * @param {float} width
+		 * @param {float} height
+		 */
+		function defineSizes(pdfView, width, height) {
+			pdfView.find('canvas').attr('width', width).attr('height', height);
+			pdfView.find('.page,.canvasWrapper,.textLayer').css('width', width + 'px').css('height', height + 'px');
 		}
-		function getAndclearCanvasContext(elm) {
-			var canvas = elm.find('canvas').get(0);
+		/**
+		 * RAZ le context du canvas et le retourne
+		 * @param {JQueryElement} pdfView
+		 * @returns {CanvasContext}
+		 */
+		function getAndClearCanvasContext(pdfView) {
+			var canvas = pdfView.find('canvas').get(0);
 			if(canvas) {
 				var ctx = canvas.getContext('2d');
 				ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -99,15 +126,22 @@
 			}
 			return null;
 		}
-		function clearTextLayer(elm) {
-			var textLayer = elm.find('.textLayer').get(0);
+		/**
+		 * RAZ le calque du texte
+		 * @param {JQueryElement} pdfView
+		 */
+		function clearTextLayer(pdfView) {
+			var textLayer = pdfView.find('.textLayer').get(0);
 			if (textLayer) {
 				textLayer.innerHTML = "";
 			}
 		}
+		/**
+		 * Controller
+		 * @param {angular $q} $q
+		 */
 		function PdfViewCtrl($q) {
 			var ctrl = this;
-			ctrl.document = null;
 			var deferred = {defer: $q.defer()};
 			ctrl.readyToRender = readyToRender;
 			ctrl.setReadyToRender = setReadyToRender;
